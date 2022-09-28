55a3a6c529b574a898169335ba673cf490c901c9
ishopjapan <thoitran2107@gmail.com>
2022-09-28 14:52:02 +0700

    demo spring boot

---
+++ src/main/java/com/macofba/demo2springboot/Demo2SpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.macofba.demo2springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo2SpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo2SpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/macofba/demo2springboot/Demo2SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macofba.demo2springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo2SpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
